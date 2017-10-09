e8db9a3cc12f0757405bbc49e4c4a1d0b03a8ea8
David Briggs <david.briggs@abaco.com>
2017-10-09 15:46:24 +0100

    Add PPC11A to set of possible boards supported

--- arch/powerpc/platforms/85xx/corenet_generic.c
+++ arch/powerpc/platforms/85xx/corenet_generic.c
@@ -172,6 +172,8 @@ static const char * const boards[] __initconst = {
 	"fsl,T1042RDB",
 	"fsl,T1042RDB_PI",
 	"keymile,kmcoge4",
+	"abaco,PPC11A-T1042",
+	"abaco,PPC11A-T2081",
 	"abaco,SBC314-T1042",
 	"abaco,SBC314-T2081",
 	NULL
